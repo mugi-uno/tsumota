1f29eabe7c089c9347db707d09f0c04399ff01aa
mugi <mugi.uno@gmail.com>
2015-10-10 13:47:41 +0900

    登録するとこまで

--- lib/collector.rb
+++ lib/collector.rb
@@ -4,21 +4,24 @@ class Collector
   # 探索基底パスを設定し初期化
   def initialize(root_path)
     # TODO : パス文字を意識せずに動くようにしたい...(windowsでも)
-    # @root_path = root_path
-    @path = Pathname.new(root_path)
+    @root_path = Pathname.new(root_path)
+
   end
 
   # 基底パス配下を探索し、ファイルごとに受け取ったprocを呼ぶ
   def collect
-    @path.each_child {|pathname|
-      yield pathname
+    @root_path.each_child {|pathname|
+      register pathname.relative_path_from(@root_path)
     }
+  end
 
-    # Dir.glob("#{@root_path}/*").each {|filename|
-    #   p "#{filename} -> #{File.ftype(filename)}"
-    #   if File.file?(filename)
-    #     yield filename
-    #   end
-    # }
+  # 降るパス
+  def register(pathname)
+    item = Item.new({relative_path: pathname.to_s})
+    if item.save
+      p "register : #{pathname.to_s}"
+    else
+      p "already exists : #{pathname.to_s}"
+    end
   end
 end
