990211d77b5e901565d6f362cadede81c605cde7
mugi <mugi.uno@gmail.com>
2015-10-10 17:39:46 +0900

    paginate

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def search
-    items = Item.matched_keyword(params[:keyword]).page(1).per(20)
+    items = Item.matched_keyword(params[:keyword]).page(params[:page]).per(20)
     render partial: 'items/paginate_list', locals: { items: items }
   end
 
   def download
     item = Item.find(params[:id])
+    item.download_count += 1
+    item.save
+
+
+    logger.info "////// DONWLOAD!!! ::: #{item.download_count}"
+
     path = Pathname.new(ROOT_PATH) + item.relative_path
     send_file(path.to_s)
   end
