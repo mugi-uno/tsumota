59e1277f344de6d57daebc9b6064f811a2ef514b
mugi <mugi.uno@gmail.com>
2015-10-11 13:37:06 +0900

    add # tags input

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_setting
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @item.tags.destroy_all
+    @item.import_tags_csv item_params[:tags]
+    if @item.save
+      render :edit
+    else
+      redirect_to item_path
+    end
   end
 
   def search
@@ -14,8 +28,21 @@ class ItemsController < ApplicationController
     item.download_count += 1
     item.save
 
-    path = Pathname.new(Setting.first.root_path) + item.relative_path
+    path = Pathname.new(@setting.root_path) + item.relative_path
     send_file(path.to_s)
   end
 
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_setting
+    @setting = Setting.first
+  end
+
+  def item_params
+    params.require(:item).permit(:tags)
+  end
 end
--- app/models/tag_item.rb
+++ app/models/tag_item.rb
@@ -1,2 +1,4 @@
 class TagItem < ActiveRecord::Base
+  belongs_to :item
+  belongs_to :tag
 end
