3070a8c221881d4a13c3fb99226dc54cafd74e66
mugi <mugi.uno@gmail.com>
2015-10-12 23:00:52 +0900

    commit

--- lib/tasks/crawler.rb
+++ lib/tasks/crawler.rb
@@ -7,31 +7,11 @@ class Tasks::Crawler
     setting = Setting.first
 
     if setting.nil?
-      p "not find setting"
+      p "setting not find"
     end
-
-    # p "run!"
-
-    # # todo : 管理画面等から登録された設定情報をすべて取得
-    # # setting = Setings.first
-
-    # # 基底ディレクトリ内の全ファイルを捜査
-    # # 未登録状態のファイルをすべて登録する
-    # Dir.glob("#{ROOT_PATH}/*").each {|filename|
-    #   p "#{filename} -> #{File.ftype(filename)}"
-    #   if File.file?(filename)
-    #     p "file !! : #{filename}"
-    #   else
-    #     p "not file !! : #{filename}"
-    #   end
-    # }
 
     collector = Collector.new setting.root_path
 
-    # 捜索した全パスに対し登録処理を実行
-    # collector.collect {|filename|
-    #   Register.register filename
-    # }
     collector.collect
   end
 end
